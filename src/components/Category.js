cd31eaf36dc9b58af9860d280ab9f2be70027c2e
Anubhab <anubhab.techie@gmail.com>
2023-03-02 17:43:42 +0530

    Added cuisine page

--- src/components/Category.js
+++ src/components/Category.js
@@ -5,29 +5,41 @@ import { NavLink, Router } from "react-router-dom";
 
 function Category() {
   return (
-    
-    <div>
-      <NavLink to={'/cuisine/Italian'}>
-        <FaPizzaSlice />
-        <h4>Italian</h4>
-      </NavLink>
+    <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center w-[60%] p-9 gap-7">
+        <NavLink
+          to={"/cuisine/Italian"}
+          className="flex w-[150px] justify-center items-center h-[75px] bg-orange-200 rounded-full hover:bg-orange-300 transition-all duration-200"
+        >
+          <FaPizzaSlice className="m-2" />
+          <h4 className="">Italian</h4>
+        </NavLink>
 
-      <NavLink to={'/cuisine/American'}>
-        <FaHamburger />
-        <h4>American</h4>
-      </NavLink>
+        <NavLink
+          to={"/cuisine/American"}
+          className="flex w-[150px] justify-center items-center h-[75px] bg-orange-200 rounded-full hover:bg-orange-300 transition-all duration-200"
+        >
+          <FaHamburger className="m-2" />
+          <h4>American</h4>
+        </NavLink>
 
-      <NavLink to={'/cuisine/Thai'}>
-        <GiNoodles />
-        <h4>Thai</h4>
-      </NavLink>
+        <NavLink
+          to={"/cuisine/Thai"}
+          className="flex w-[150px] justify-center items-center h-[75px] bg-orange-200 rounded-full hover:bg-orange-300 transition-all duration-200"
+        >
+          <GiNoodles className="m-2" />
+          <h4>Thai</h4>
+        </NavLink>
 
-      <NavLink to={'/cuisine/Chinese'}>
-        <GiChopsticks />
-        <h4>Chinese</h4>
-      </NavLink>
+        <NavLink
+          to={"/cuisine/Chinese"}
+          className="flex w-[150px] justify-center items-center h-[75px] bg-orange-200 rounded-full hover:bg-orange-300 transition-all duration-200"
+        >
+          <GiChopsticks className="m-2" />
+          <h4>Chinese</h4>
+        </NavLink>
+      </div>
     </div>
-
   );
 }
 
--- src/pages/Cuisine.js
+++ src/pages/Cuisine.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import {Link, useParams} from 'react-router-dom';
 import { motion } from "framer-motion";
+import { SplideSlide } from "@splidejs/react-splide";
 
 function Cuisine() {
 
@@ -12,16 +13,33 @@ function Cuisine() {
     const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}&number=10`)
     const data=await response.json()
     setCuisine(data.results)
+    console.log(cuisine)
   }
 
   useEffect(() => {
     getCuisine(params.name)
-    console.log(params.name)
+    
   }, [params])
 
 
-  return <div>
-
+  return <div className="flex gap-5 flex-wrap justify-center">
+    {cuisine.map((recipe) => {
+      return (
+        
+        <div key={recipe.id} className="mt-[50px]">
+                
+                <img
+                  className="rounded-xl opacity-80 cursor-pointer hover:opacity-100 transition-opacity  object-cover w-[250px] h-[250px] mt-3"
+                  src={recipe.image}
+                  alt={recipe.title}
+                />
+                
+                <h1 className="h-10 font-bold w-[100%] m-5">{(recipe.title).substring(0,25)+'...'}</h1>
+              
+            </div>
+        
+      );
+    })}
   </div>;
 }
 
